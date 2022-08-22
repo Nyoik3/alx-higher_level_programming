db0a0bb47b50a69c6eb056042c0809b21c2f58f5
Nyoik3 <89342740+Nyoik3@users.noreply.github.com>
2022-08-22 16:36:08 +0300

    pyfile7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,11 @@
+[A[B#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_wordword_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1][B[A[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[B[B[C[C[C[C[C[C[C[C[C[C[C[A[D[D[D[D[D[D[D[D[D[D
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
