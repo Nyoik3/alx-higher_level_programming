a47741a27331394dfcb3ec12d9ee0b174d85480d
Nyoik3 <89342740+Nyoik3@users.noreply.github.com>
2022-08-22 15:56:50 +0300

    integer

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 98
+print(f"{number:d} "Battery street[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[3~:wq
