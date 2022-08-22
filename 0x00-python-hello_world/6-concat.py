7af95f000158c86bfa0256dd04dfa452847b45ce
Nyoik3 <89342740+Nyoik3@users.noreply.github.com>
2022-08-22 16:31:09 +0300

    pyfile6

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1}!")[A[C[C[C[C[C[Cstr += ' ' + str2[B[C {}!".format(str1))
